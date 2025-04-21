e24c50c3a521c66663a596584e1428923872d639
Kalyansing2025 <kalyansing.chanda@waiin.com>
2025-04-21 19:20:19 +0530

    feat: add demo component with routing and integrate into navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LeadManagementComponent } from './lead-management/lead-management.component';
+import { DemoComponent } from './demo/demo.component';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: LeadManagementComponent }, 
   { path: 'lead', component: LeadManagementComponent },
+  {path:'demo', component: DemoComponent},
+  
 
 ];
